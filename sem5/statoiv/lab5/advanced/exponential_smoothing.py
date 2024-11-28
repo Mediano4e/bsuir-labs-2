16015ddfce8fbe6b62e9748d7057244c01921c7b
Mediano4e <egor110704@gmail.com>
2024-11-28 05:59:44 +0300

    statoiv lab5 updated

--- sem5/statoiv/lab5/advanced/exponential_smoothing.py
+++ sem5/statoiv/lab5/advanced/exponential_smoothing.py
@@ -1,38 +1,65 @@
 import numpy as np
 
+
 class ExponentialSmoothing:
-    def __init__(self, alpha=0.2, beta=0.1, gamma=0.1, season_length=12):
+    def __init__(self, data, seasonal_periods, alpha=0.2, beta=0.1, gamma=0.1):
+        self.data = np.array(data)
+        self.seasonal_periods = seasonal_periods
         self.alpha = alpha
         self.beta = beta
         self.gamma = gamma
-        self.season_length = season_length
+        self.level = None
+        self.trend = None
+        self.seasonality = None
+        self.fitted_values = []
+        self.forecast_values = []
     
-    def fit(self, train):
-        self.train = np.array(train)
-        self.n = len(self.train)
+    def initialize_components(self):
+        self.level = np.mean(self.data[:self.seasonal_periods])
         
-        self.level = np.zeros(self.n)
-        self.trend = np.zeros(self.n)
-        self.seasonal = np.zeros(self.n)
-
-        self.level[0] = self.train[0]
-        self.trend[0] = self.train[1] - self.train[0]
-        self.seasonal[:self.season_length] = self.train[:self.season_length] - np.mean(self.train[:self.season_length])
-
-        for t in range(1, self.n):
-            if t < self.season_length:
-                self.seasonal[t] = self.train[t] - self.level[t-1]
+        self.trend = (np.mean(self.data[self.seasonal_periods:2*self.seasonal_periods]) - 
+                      np.mean(self.data[:self.seasonal_periods])) / self.seasonal_periods
+        
+        self.seasonality = np.zeros(self.seasonal_periods)
+        for i in range(self.seasonal_periods):
+            self.seasonality[i] = np.mean(self.data[i::self.seasonal_periods]) - self.level
+    
+    def fit(self):
+        self.initialize_components()
+        n = len(self.data)
+        level = self.level
+        trend = self.trend
+        seasonality = self.seasonality
+        fitted_values = []
+        
+        for t in range(n):
+            if t >= self.seasonal_periods:
+                seasonal_component = seasonality[t % self.seasonal_periods]
             else:
-                self.seasonal[t] = self.gamma * (self.train[t] - self.level[t-1]) + (1 - self.gamma) * self.seasonal[t-self.season_length]
+                seasonal_component = 0
             
-            self.level[t] = self.alpha * (self.train[t] - self.seasonal[t]) + (1 - self.alpha) * (self.level[t-1] + self.trend[t-1])
-            self.trend[t] = self.beta * (self.level[t] - self.level[t-1]) + (1 - self.beta) * self.trend[t-1]
-
-    def forecast(self, forecast_length):
+            fitted = level + trend + seasonal_component
+            fitted_values.append(fitted)
+            
+            if t >= self.seasonal_periods:
+                error = self.data[t] - fitted
+                new_level = self.alpha * (self.data[t] - seasonal_component) + (1 - self.alpha) * (level + trend)
+                new_trend = self.beta * (new_level - level) + (1 - self.beta) * trend
+                new_seasonality = self.gamma * error + (1 - self.gamma) * seasonal_component
+                
+                level, trend = new_level, new_trend
+                seasonality[t % self.seasonal_periods] = new_seasonality
+        
+        self.level = level
+        self.trend = trend
+        self.seasonality = seasonality
+        self.fitted_values = np.array(fitted_values)
+    
+    def forecast(self, steps):
         forecasts = []
-        for h in range(1, forecast_length + 1):
-            forecast = self.level[-1] + h * self.trend[-1] + self.seasonal[(self.n + h - self.season_length) % self.n]
+        for h in range(steps):
+            seasonal_component = self.seasonality[(len(self.data) + h) % self.seasonal_periods]
+            forecast = self.level + h * self.trend + seasonal_component
             forecasts.append(forecast)
-        return np.array(forecasts)
-
-
+        self.forecast_values = np.array(forecasts)
+        return self.forecast_values
